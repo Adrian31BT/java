36441b266db942000ab34982dd389f95532c4f94
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-20 15:16:39 -0500

    reto15: implementado clase Validacion y TestValidacion

---
+++ Repaso/src/com/cmc/repaso/test/TestValidacion.java
@@ -0,0 +1,16 @@
+package com.cmc.repaso.test;
+
+import com.cmc.repaso.entidades.Validacion;
+
+public class TestValidacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Validacion prueba = new Validacion();
+		boolean respuesta1 = prueba.validarMonto(0);
+		boolean respuesta2 = prueba.validarMonto(10);
+		System.out.println(respuesta1);
+		System.out.println(respuesta2);
+	}
+
+}
