ff7e6e3230ee52e1edc706b70335b732a31cb8c9
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-23 10:54:04 -0500

    reto21: Ejercicio 1 completado: implementado clases Carta, Numero y Palos

---
+++ Cartas/src/entidades/Carta.java
@@ -0,0 +1,30 @@
+package entidades;
+
+public class Carta {
+	private Numero numero;
+	private String palo;
+	
+	public Carta(Numero numero, String palo) {
+		this.numero = numero;
+		this.palo = palo;
+	}
+	
+	public void mostrar() {
+		System.out.println(numero+"-"+palo);
+	}
+	
+	public Numero getNumero() {
+		return numero;
+	}
+	public void setNumero(Numero numero) {
+		this.numero = numero;
+	}
+	public String getPalo() {
+		return palo;
+	}
+	public void setPalo(String palo) {
+		this.palo = palo;
+	}
+	
+	
+}
