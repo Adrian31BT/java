0d62b2a1cd5d996897affd82d0a6c2cfab679e01
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-24 12:09:33 -0500

    reto22: Ejercicio 1 completado, se implemento el calculo de amortizacion

---
+++ ADRIAN_BACILIO_Amortizacion/src/com/cmc/evaluacion/servicios/CalculadoraAmortizacion.java
@@ -0,0 +1,17 @@
+package com.cmc.evaluacion.servicios;
+
+import com.cmc.evaluacion.entidades.Prestamo;
+
+public class CalculadoraAmortizacion {
+	public static double calcularCuota(Prestamo prestamo) {
+		double cuota;
+		double i;
+		double dividendo;
+		double divisor;
+		i = (prestamo.getInteres()/12)/100;
+		dividendo = prestamo.getMonto() * i;
+		divisor = 1 - Math.pow((1+i), -(prestamo.getPlazo()));
+		cuota = dividendo/divisor;
+		return cuota;
+	}
+}
---
+++ ADRIAN_BACILIO_Amortizacion/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ADRIAN_BACILIO_Amortizacion {
+}
