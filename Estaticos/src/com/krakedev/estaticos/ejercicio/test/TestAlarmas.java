9778fabb0a7dc931267020494541104ee2952cfe
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-28 13:12:39 -0500

    reto28, se implementaron las clases de Alarma, AdminAlarmas, DiasSemana(estatico) y TestAlarmas y se mostro por consola las alarmas instanciadas

---
+++ Estaticos/src/com/krakedev/estaticos/ejercicio/test/TestAlarmas.java
@@ -0,0 +1,38 @@
+package com.krakedev.estaticos.ejercicio.test;
+
+import java.util.ArrayList;
+
+import com.krakedev.estaticos.ejercicio.entidades.Alarma;
+import com.krakedev.estaticos.ejercicio.logica.AdminAlarmas;
+import com.krakedev.estaticos.ejercicio.utils.DiasSemana;
+
+public class TestAlarmas {
+
+	public static void main(String[] args) {
+		
+		DiasSemana ds = new DiasSemana();
+		
+		Alarma a0 = new Alarma(ds.LUNES, 7, 30);
+		Alarma a1 = new Alarma(ds.MARTES, 7, 30);
+		Alarma a2 = new Alarma(ds.MIERCOLES, 7, 30);
+		Alarma a3 = new Alarma(ds.JUEVES, 7, 30);
+		Alarma a4 = new Alarma(ds.VIERNES, 7, 30);
+		Alarma a5 = new Alarma(ds.SABADO, 9, 30);
+		Alarma a6 = new Alarma(ds.DOMINGO, 10, 30);
+		
+		AdminAlarmas admA = new AdminAlarmas();
+		admA.agregarAlarma(a0);
+		admA.agregarAlarma(a1);
+		admA.agregarAlarma(a2);
+		admA.agregarAlarma(a3);
+		admA.agregarAlarma(a4);
+		admA.agregarAlarma(a5);
+		admA.agregarAlarma(a6);
+		
+		ArrayList<Alarma> alarmasActuales = admA.getAlarmas();
+		System.out.println(alarmasActuales);
+		
+		
+	}
+
+}
---
+++ Estaticos/src/com/krakedev/estaticos/ejercicio/utils/DiasSemana.java
@@ -0,0 +1,11 @@
+package com.krakedev.estaticos.ejercicio.utils;
+
+public class DiasSemana {
+	public final int LUNES = 0;
+	public final int MARTES = 1;
+	public final int MIERCOLES = 2;
+	public final int JUEVES = 3;
+	public final int VIERNES = 4;
+	public final int SABADO = 5;
+	public final int DOMINGO = 6;
+}
