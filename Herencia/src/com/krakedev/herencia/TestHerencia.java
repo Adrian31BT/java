b847ea7c68973aa866bb37b291cb0c8b549d135e
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-28 11:47:00 -0500

    reto27: ejercicio de herencia clase Gato

--- Herencia/src/com/krakedev/herencia/TestHerencia.java
+++ Herencia/src/com/krakedev/herencia/TestHerencia.java
@@ -5,7 +5,11 @@ public class TestHerencia {
 	public static void main(String[] args) {
 		Perro p  = new Perro();
 		p.dormir();
-		//p.ladrar();
+		
+		System.out.println("\n-----Reto 27------");
+		Gato g = new Gato();
+		g.dormir();
+		
 	}
 
 }
