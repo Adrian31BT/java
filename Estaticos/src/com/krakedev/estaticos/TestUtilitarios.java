172bd3323b1c239b5941db623d71cd5fdec161fa
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-28 12:17:08 -0500

    se implementaron ejercicios de java - estaticos

---
+++ Estaticos/src/com/krakedev/estaticos/TestUtilitarios.java
@@ -0,0 +1,10 @@
+package com.krakedev.estaticos;
+
+public class TestUtilitarios {
+
+	public static void main(String[] args) {
+		boolean resp = Utilitarios.validarRango(20);
+		System.out.println(resp);
+	}
+
+}
