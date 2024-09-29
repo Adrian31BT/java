3b4d314cf4903d8e74452bc9c94c0e03d7b5fd90
Adrian31BT <adrian31baciliot@gmail.com>
2024-09-28 21:32:08 -0500

    reto30: se implemento el proyecto maven de ejemplojunit

---
+++ ejemplojunit/src/main/java/com/krakedev/ejemplojunit/Calculadora.java
@@ -0,0 +1,15 @@
+package com.krakedev.ejemplojunit;
+
+public class Calculadora {
+	public int sumar(int numero1, int numero2) {
+		int suma;
+		suma = numero1 + numero2;
+		return suma;
+	}
+	
+	public int restar(int numero1, int numero2) {
+		int restar;
+		restar = numero1 - numero2;
+		return restar;
+	}
+}
