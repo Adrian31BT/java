72a487fc2a64a6f13913fdb40822b10d017cd1b7
Adrian31BT <adrian31baciliot@gmail.com>
2024-10-02 11:46:28 -0500

    reto40: impleentado la mejora de codigo en el DataSource

--- ejemeploweb/src/main/java/com/krakedev/persistencia/ClientesBDD.java
+++ ejemeploweb/src/main/java/com/krakedev/persistencia/ClientesBDD.java
@@ -1,18 +1,44 @@
 package com.krakedev.persistencia;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import com.krakedev.entidades.Cliente;
+import com.krakedev.excepciones.KrakeDevException;
 import com.krakedev.utils.ConexionBDD;
 
 public class ClientesBDD {
 	
-	public void insertar(Cliente cliente) {
-		Connection con = ConexionBDD.obtenerConexion();
-		if(con != null) {
-			System.out.println("Conectado");
-		}else {
-			System.out.println("Error al obtener conexion");
-		}
+	public void insertar(Cliente cliente) throws KrakeDevException{
+		Connection con = null; 
+		try {
+			con = ConexionBDD.obtenerConexion();
+			PreparedStatement ps = con.prepareStatement("insert into clientes(cedula, nombre, numerohijos) values (?,?,?)");
+			ps.setString(1, cliente.getCedula());
+			ps.setString(2, cliente.getNombre());
+			ps.setInt(3, cliente.getNumeroHijos());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new KrakeDevException("Error al insertar cliente");
+		} catch (KrakeDevException e) {
+			throw e;
+		};
+	}
+	
+	public void actualizar(Cliente cliente) throws KrakeDevException{
+		Connection con = null; 
+		try {
+			con = ConexionBDD.obtenerConexion();
+			PreparedStatement ps = con.prepareStatement("update clientes set nombre = ?, numerohijos = ? where cedula = ?");
+			ps.setString(1, cliente.getNombre());
+			ps.setInt(2, cliente.getNumeroHijos());
+			ps.setString(3, cliente.getCedula());
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			throw new KrakeDevException("Error al actualizar cliente");
+		} catch (KrakeDevException e) {
+			throw e;
+		};
 	}
 }
--- ejemeploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
+++ ejemeploweb/src/main/java/com/krakedev/servicios/ServicioClientes.java
@@ -7,8 +7,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.krakedev.entidades.Cliente;
+import com.krakedev.excepciones.KrakeDevException;
 import com.krakedev.persistencia.ClientesBDD;
 
 @Path("clientes")
@@ -30,15 +32,30 @@ public class ServicioClientes {
 	@Path("insertar")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void insertar(Cliente cliente) {
-		System.out.println(">>>"+ cliente);
+	public Response insertar(Cliente cliente) {
+		System.out.println("INSERTANDO >>>"+ cliente);
 		ClientesBDD cli = new ClientesBDD();
-		cli.insertar(cliente);
+		try {
+			cli.insertar(cliente);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
 	}
 	@Path("actualizar")
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void actualizar(Cliente cliente) {
+	public Response actualizar(Cliente cliente) {
 		System.out.println("ACTUALIZANDO >>>"+ cliente);
+		ClientesBDD cli = new ClientesBDD();
+		try {
+			cli.actualizar(cliente);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+		
 	}
 }
